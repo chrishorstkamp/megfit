fba8b06f8e3d9229655cbc6ce5914ab2cb1f544b
Chris Horstkamp <horstkampc@gmail.com>
2026-04-26 12:37:25 -0400

    Cleaned svelte config and fixed service worker pathing

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
+    preprocess: [vitePreprocess({})],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		serviceWorker: { register: false },
-		files: { serviceWorker: 'src/service-worker.ts' },
-		alias: { '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js' }
-	}
+    kit: {
+        adapter: adapter(),
+        // SvelteKit 2 automatically detects src/service-worker.ts. 
+        // We set register to false because the PWA plugin handles it.
+        serviceWorker: { register: false },
+        alias: { '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js' }
+    }
 };
 
 export default config;
